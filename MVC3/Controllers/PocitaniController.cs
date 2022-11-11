46c0f62c60022601553e0f1df2f2fdfbf574a094
Alfred <kufaafred@gmail.com>
2022-11-11 21:18:58 +0100

    Prvni fungujici verze aplikace

---
+++ MVC3/Controllers/PocitaniController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC3.Models;
+
+namespace MVC3.Controllers
+{
+    public class PocitaniController : Controller
+    {
+        public IActionResult Index()
+        {
+            var kuryr1 = new KuryrViewModel();
+         
+            return View(kuryr1);
+        }
+
+       public IActionResult VytvorKuryra(KuryrViewModel c)
+        {
+            c.vysledek = c.penezenka + c.dyska - c.nakupovani - c.tankovani - c.trzba;
+            return View("Vysledek", c);
+
+        }
+
+       
+    }
+}
---
+++ MVC3/Models/KuryrViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC3.Models
+{
+	public class KuryrViewModel
+	{
+		public string jmeno { get; set; }
+		public int trzba { get; set; }
+		public int dyska { get; set; }
+		public int penezenka { get; set; }
+		public int nakupovani { get; set; }
+		public int tankovani { get; set; }
+		public int vysledek { get; set; }
+
+	}
+}
